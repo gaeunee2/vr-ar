cc435b730b123ce14ddb64eac1cedfe96073cee1
gaeunee2 <kaeune3@gmail.com>
2024-10-12 01:44:41 +0900

    2024-10-12
    CatEscape Finish

--- CatEscape/Assets/PlayerController.cs
+++ CatEscape/Assets/PlayerController.cs
@@ -10,14 +10,27 @@ public class PlayerController : MonoBehaviour
         Application.targetFrameRate = 60;
     }
 
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
     // Update is called once per frame
     void Update()
     {
         //왼쪽 화살표
         if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             transform.Translate(-3, 0, 0);
+        }
         //오른쪽 화살표
         if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             transform.Translate(3, 0, 0);
+        }
     }
 }
