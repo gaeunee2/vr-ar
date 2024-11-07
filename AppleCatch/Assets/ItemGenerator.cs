02408fa53dd49e2de50f5b9dced8ca9d0f8c9bff
gaeunee2 <kaeune3@gmail.com>
2024-11-07 17:41:44 +0900

    2024-11-07
    new Project
    AppleCatch! (unfinished)

---
+++ AppleCatch/Assets/ItemGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject apple = Instantiate(applePrefab);
+            GameObject bomb = Instantiate(bombPrefab);
+
+            int px = Random.Range(Vector3())
+        }
+    }
+}
